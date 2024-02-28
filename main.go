055474c868bb44845daa4e5085ba3dbcb29153f0
mind-changer <palbanagar@gmail.com>
2024-02-28 20:24:47 +0530

    feat: add main.go with go mod files

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/lite-social-presence-system/config"
+	"github.com/lite-social-presence-system/internal/server"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	var err error
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetReportCaller(true)
+	cfg := &config.Config{}
+	err = config.LoadConfigFromEnv(cfg, ".env")
+	if err != nil {
+		logrus.Fatal("SERVER QUIT ERROR- Error while loading env config ", err.Error())
+		return
+	}
+
+	go server.RunHTTPServer(cfg)
+	server.RunGRPCServer(cfg)
+}
